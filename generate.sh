dd0d5ef39eaf7a757eeddb02dfd01fbc2ac6f150
Divine Threepwood <divine@openbase.org>
2019-05-18 13:02:19 +0200

    setup swagger codegen

--- generate.sh
+++ generate.sh
@@ -1,4 +1,12 @@
 
 
-java -jar ../swagger-codegen/modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate -i api/bco-openapi.yaml -o client-jaxrs-jersey -l jaxrs-jersey
-java -jar ../swagger-codegen/modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate -i api/bco-openapi.yaml -o client-jaxrs-jersey -l jaxrs-jersey
+# prepare swagger
+git submodule init
+git submodule update
+cd swagger-codegen
+git checkout v3.0.8
+mvn install
+cd ..
+
+java -jar swagger-codegen/modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate -i api/bco-openapi.yaml -o client-jaxrs-jersey -l jaxrs-jersey
+#java -jar swagger-codegen/modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate -i api/bco-openapi.yaml -o client-jaxrs-jersey -l jaxrs-jersey
