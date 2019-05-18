59dfed163002163707dba79ca41185e6fae37b0c
Divine Threepwood <divine@openbase.org>
2019-05-18 12:50:45 +0200

    start building project structure

---
+++ generate.sh
@@ -0,0 +1,4 @@
+
+
+java -jar ../swagger-codegen/modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate -i api/bco-openapi.yaml -o client-jaxrs-jersey -l jaxrs-jersey
+java -jar ../swagger-codegen/modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate -i api/bco-openapi.yaml -o client-jaxrs-jersey -l jaxrs-jersey
