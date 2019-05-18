1ec76f4ce7e8627bcc8788558c5ae40b597dd194
Divine Threepwood <divine@openbase.org>
2019-05-18 14:55:37 +0200

    Implement light controller prototype

--- client-spring/src/main/java/io/swagger/api/LightApiController.java
+++ client-spring/src/main/java/io/swagger/api/LightApiController.java
@@ -3,6 +3,13 @@ package io.swagger.api;
 import io.swagger.model.LightData;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.annotations.*;
+import io.swagger.model.PowerState;
+import org.openbase.bco.dal.remote.layer.unit.ColorableLightRemote;
+import org.openbase.bco.dal.remote.layer.unit.Units;
+import org.openbase.bco.registry.remote.Registries;
+import org.openbase.jul.exception.NotAvailableException;
+import org.openbase.type.domotic.state.PowerStateType;
+import org.openbase.type.domotic.unit.UnitTemplateType.UnitTemplate.UnitType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -21,6 +28,7 @@ import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
+
 @javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2019-05-18T13:17:07.324917+02:00[Europe/Berlin]")
 @Controller
 public class LightApiController implements LightApi {
@@ -35,16 +43,31 @@ public class LightApiController implements LightApi {
     public LightApiController(ObjectMapper objectMapper, HttpServletRequest request) {
         this.objectMapper = objectMapper;
         this.request = request;
+
+
     }
 
     public ResponseEntity<LightData> lightGet() {
-        String accept = request.getHeader("Accept");
-        return new ResponseEntity<LightData>(HttpStatus.NOT_IMPLEMENTED);
+        try {
+            final ColorableLightRemote unitRemote = Units.getUnitByAlias("ColorableLight-9", true, Units.COLORABLE_LIGHT);
+            return ResponseEntity.ok(new LightData()
+                    .id(unitRemote.getId())
+                    .powerState(new PowerState().value(unitRemote.getPowerState().getValue().name())));
+        } catch (Exception e) {
+            return new ResponseEntity<LightData>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        //String accept = request.getHeader("Accept");
+
+
     }
 
-    public ResponseEntity<Void> lightPost(@ApiParam(value = "" ,required=true )  @Valid @RequestBody LightData body) {
-        String accept = request.getHeader("Accept");
-        return new ResponseEntity<Void>(HttpStatus.NOT_IMPLEMENTED);
+    public ResponseEntity<Void> lightPost(@ApiParam(value = "", required = true) @Valid @RequestBody LightData body) {
+        try {
+            Units.getUnitByAlias("ColorableLight-9", true, Units.COLORABLE_LIGHT).setPowerState(PowerStateType.PowerState.State.valueOf(body.getPowerState().getValue()));
+            return ResponseEntity.ok(null);
+        } catch (Exception e) {
+            return new ResponseEntity<Void>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
 }
