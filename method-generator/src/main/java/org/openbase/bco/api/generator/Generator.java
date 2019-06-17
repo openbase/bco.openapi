2e6a8e49f99285c64a6041cf7c790bf7813e6481
Tamino Huxohl <thuxohl@techfak.uni-bielefeld.de>
2019-06-17 16:46:31 +0200

    a lot of work on method generation

--- method-generator/src/main/java/org/openbase/bco/api/generator/Generator.java
+++ method-generator/src/main/java/org/openbase/bco/api/generator/Generator.java
@@ -2,16 +2,24 @@ package org.openbase.bco.api.generator;
 
 import com.google.protobuf.Message;
 import com.google.protobuf.ProtocolMessageEnum;
+import org.eclipse.jgit.lib.ObjectIdOwnerMap;
 import org.openbase.bco.registry.template.lib.TemplateRegistry;
+import org.openbase.jul.exception.CouldNotPerformException;
+import org.openbase.jul.exception.NotAvailableException;
+import org.openbase.jul.exception.printer.ExceptionPrinter;
 import org.openbase.jul.iface.Shutdownable;
 import org.openbase.jul.pattern.provider.DataProvider;
 import org.openbase.jul.storage.registry.RegistryService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.yaml.snakeyaml.DumperOptions;
 import org.yaml.snakeyaml.Yaml;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileWriter;
 import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.util.*;
@@ -50,161 +58,157 @@ public class Generator {
             RegistryService.class
     };
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(Generator.class);
+
     public static void main(final String[] args) {
 
-        parseRegistryMethods(TemplateRegistry.class);
+        final DumperOptions options = new DumperOptions();
+        options.setDefaultFlowStyle(DumperOptions.FlowStyle.BLOCK);
+        final Yaml yaml = new Yaml(options);
 
         try {
-            /*YamlInput yamlInput = Yaml.createYamlInput(new File("/home/thuxohl/workspace/openbase/bco.openapi/api/bco-openapi.yaml"));
-            YamlMapping yamlMapping = yamlInput.readYamlMapping();
-            System.out.println(yamlMapping.toString());*/
-            Yaml yaml = new Yaml();
-            try (FileInputStream stream = new FileInputStream(new File("/home/thuxohl/workspace/openbase/bco.openapi/api/bco-openapi.yaml"))) {
-                Map<String, Object> map = (Map) yaml.load(stream);
+            final File file = new File("/home/thuxohl/workspace/openbase/bco.openapi/api/bco-openapi.yaml");
+            final Map<String, Object> apiDefinition;
+            try (FileInputStream stream = new FileInputStream(file)) {
+                apiDefinition = (Map) yaml.load(stream);
+            }
 
-                //System.out.println(yaml.dump(getAs(map, "paths", Map.class)));
+            for (Map.Entry<String, Object> entry : parseRegistryMethods(TemplateRegistry.class).entrySet()) {
+                ((Map<String, Object>) apiDefinition.get("paths")).put(entry.getKey(), entry.getValue());
+            }
+
+            try(FileWriter fileWriter = new FileWriter(file)) {
+                fileWriter.write(yaml.dump(apiDefinition));
             }
         } catch (Exception ex) {
-            System.out.println("Error: " + ex);
+            ExceptionPrinter.printHistory(ex, LOGGER);
         }
     }
 
-    private static Map<String, Object> getMap(final Map<String, Object> map, final String key) {
-        return (Map<String, Object>) map.get(key);
+    private static Set<Method> methodsToIgnore() {
+        final Set<Method> methodSet = new HashSet<>();
+        for (Class clazz : INTERFACES_TO_IGNORE) {
+            methodSet.addAll(Arrays.asList(clazz.getMethods()));
+        }
+        return methodSet;
     }
 
-    private static String getString(final Map<String, Object> map, final String key) {
-        return (String) map.get(key);
+    private static Map<String, Object> parseRegistryMethods(final Class registryInterface) throws CouldNotPerformException {
+        final Set<Method> methodsToIgnore = methodsToIgnore();
+
+        final Map<String, Object> result = new HashMap<>();
+        final String pathPrefix = "/registry/" + registryInterface.getSimpleName().toLowerCase().replace("registry", "");
+
+        final Set<String> methodNameSet = new HashSet<>();
+        for (Method method : registryInterface.getMethods()) {
+            if (methodsToIgnore.contains(method)) {
+                continue;
+            }
+
+            if (methodNameSet.contains(method.getName())) {
+                throw new CouldNotPerformException("Could not add method[" + method.getName() + "] for [" + registryInterface.getSimpleName() + "] because method with same name is defined more than once!");
+            }
+            methodNameSet.add(method.getName());
+
+            addMethodEntry(result, method, pathPrefix);
+        }
+
+        return result;
     }
 
-    private static <T> T getAs(final Map<String, ?> map, final String key, final Class<T> clazz) {
-        return (T) map.get(key);
+    private static void addMethodEntry(final Map<String, Object> pathMap, final Method method, final String pathPrefix) throws CouldNotPerformException {
+        final Map<String, Object> methodMap = addObject(pathPrefix + "/" + method.getName(), pathMap);
+
+        final Map<String, Object> methodTypeMap;
+        if (method.getParameters().length > 0) {
+            methodTypeMap = addObject("post", methodMap);
+            addParameters(methodTypeMap, method);
+        } else {
+            methodTypeMap = addObject("get", methodMap);
+        }
+
+        addResponse(methodTypeMap, method);
     }
 
-    private static void resolveGenerics(Method method) {
-        List<Class> list = new ArrayList<Class>();
-        if (method.getGenericReturnType() instanceof ParameterizedType) {
-            for (Type actualTypeArgument : ((ParameterizedType) method.getGenericReturnType()).getActualTypeArguments()) {
-                if (actualTypeArgument instanceof Class) {
-                    System.out.println(((Class) actualTypeArgument).getSimpleName());
-                } else {
-                    System.out.println("ERROR: " + actualTypeArgument + " is not a Class!");
-                }
+    private static void addParameters(final Map<String, Object> methodTypeMap, final Method method) throws CouldNotPerformException {
+        final Map<String, Object> requestBody = addObject("requestBody", methodTypeMap);
+        requestBody.put("required", true);
+
+        final Map<String, Object> content = addObject("content", requestBody);
+        final Map<String, Object> applicationJson = addObject("application/json", content);
+        final Map<String, Object> schema = addObject("schema", applicationJson);
+
+        if (method.getParameters().length > 1) {
+            schema.put("type", "object");
+            final Map<String, Object> properties = addObject("properties", schema);
+
+            for (final Class parameter : method.getParameterTypes()) {
+                addType(addObject(getParameterName(parameter, method.getName()), properties), parameter);
             }
         } else {
-            System.out.println("NOT parametrized: " + method.getName() + ", " + method.getGenericReturnType());
+            addType(schema, method.getParameterTypes()[0]);
         }
     }
 
-    private static void parseRegistryMethods(final Class registryClass) {
-        final Set<Method> methodsToIgnore = new HashSet<>();
-
-        for (Class anInterface : registryClass.getInterfaces()) {
-            for (Class aClass : INTERFACES_TO_IGNORE) {
-                if (aClass.equals(anInterface)) {
-                    for (Method method : anInterface.getMethods()) {
-                        methodsToIgnore.add(method);
-                    }
-                }
+    private static String getParameterName(final Class parameter, final String methodName) {
+        if (parameter.getClasses().equals(String.class)) {
+            if (methodName.split("By").length > 1) {
+                return methodName.split("By")[1].substring(0, 1) + methodName.split("By")[1].substring(1);
             }
         }
+        return parameter.getSimpleName().substring(0, 1).toLowerCase() + parameter.getSimpleName().substring(1);
+    }
 
-        Map<String, Object> pathMap = new HashMap<>();
-        pathMap.put("post", new HashMap<>());
-        Map<String, Object> post = getMap(pathMap, "post");
-        post.put("requestBody", new HashMap<>());
-        Map<String, Object> requestBody = getMap(post, "requestBody");
-        requestBody.put("required", true);
-        requestBody.put("content", new HashMap<>());
+    private static void addResponse(final Map<String, Object> methodTypeMap, final Method method) throws CouldNotPerformException {
+        final Map<String, Object> responses = addObject("responses", methodTypeMap);
+        final Map<String, Object> response200 = addObject("200", responses);
+        final Map<String, Object> content = addObject("content", response200);
+        final Map<String, Object> applicationJson = addObject("application/json", content);
+        final Map<String, Object> schema = addObject("schema", applicationJson);
 
-        post.put("responses", new HashMap<>());
-        Map<String, Object> responses = getMap(post, "responses");
-        responses.put("200", new HashMap<>());
-        Map<String, Object> map = getMap(responses, "200");
-        map.put("description", "Success");
-        map.put("content", new HashMap<>());
-        Map<String, Object> content = getMap(map, "content");
-        //TODO: handle request values, probably as anyOf, add method name as text/plain?
-
-        content.put("application/json", new HashMap<>());
-        Map<String, Object> json = getMap(content, "application/json");
-        json.put("schema", new HashMap<>());
-        Map<String, Object> schema = getMap(json, "schema");
-        schema.put("oneOf", new ArrayList<>());
-        List oneOf = getAs(schema, "oneOf", List.class);
-
-        Map<Type, Class> returnTypes = new HashMap<>();
-        for (Method method : registryClass.getMethods()) {
-            if (!methodsToIgnore.contains(method)) {
-                returnTypes.put(method.getGenericReturnType(), method.getReturnType());
-                String a = "Add method: " + method.getName() + " returns ";
-                if (method.getReturnType().equals(List.class) || method.getReturnType().equals(Future.class)) {
-                    a += method.getReturnType().getSimpleName() + "<";
-                    a += resolveType(((Class) ((ParameterizedType) method.getGenericReturnType()).getActualTypeArguments()[0]));
-                    a += "> params ";
-                } else {
-                    a += resolveType(method.getReturnType()) + " params ";
-                }
-
-                /*for (int i = 0; i < method.getParameterTypes().length; i++) {
-                    if (i == method.getParameterTypes().length - 1) {
-                        a += resolveType(method.getParameterTypes()[i]);
-                    } else {
-                        a += resolveType(method.getParameterTypes()[i]) + ", ";
-                    }
-                }*/
-                System.out.println(a);
-            }
+        if (method.getReturnType().equals(List.class)) {
+            final Class actualTypeArgument = (Class) ((ParameterizedType) method.getGenericReturnType()).getActualTypeArguments()[0];
+            schema.put("type", "array");
+            addType(addObject("items", schema), actualTypeArgument);
+        } else if (method.getReturnType().equals(Future.class)) {
+            final Class actualTypeArgument = (Class) ((ParameterizedType) method.getGenericReturnType()).getActualTypeArguments()[0];
+            addType(schema, actualTypeArgument);
+        } else {
+            addType(schema, method.getReturnType());
         }
+    }
 
-        //TODO: handle primitives (string and boolean, as text/plain?)
-        //TODO: handle duplicate booleans through Boolean.class and boolean
-        Set<Class> handledClasses = new HashSet<>();
-        for (Type returnType : returnTypes.keySet()) {
-            System.out.println("Return type: " + returnType);
-            HashMap<String, Object> schemaMap = new HashMap<>();
+    private static Map<String, Object> addObject(final String name, final Map<String, Object> objectToAddTo) {
+        final Map<String, Object> map = new HashMap<>();
+        objectToAddTo.put(name, map);
+        return map;
+    }
 
-            if(returnTypes.get(returnType).equals(List.class)) {
-                Class actualTypeArgument1 = (Class) ((ParameterizedType) returnType).getActualTypeArguments()[0];
-                Class actualTypeArgument = actualTypeArgument1;
-                schemaMap.put(actualTypeArgument.getSimpleName() + "List", new HashMap<>());
-                Map<String, Object> map1 = getMap(schemaMap, actualTypeArgument.getSimpleName() + "List");
-                map1.put("type", "array");
-                map1.put("items", new HashMap<>());
-                Map<String, Object> items = getMap(map1, "items");
-                items.put("$ref", resolveType(actualTypeArgument));
-            } else if(returnTypes.get(returnType).equals(Future.class)) {
+    private static boolean isReferencedType(final Class clazz) {
+        return Message.class.isAssignableFrom(clazz) || ProtocolMessageEnum.class.isAssignableFrom(clazz);
+    }
 
-                Class actualTypeArgument1 = (Class) ((ParameterizedType) returnType).getActualTypeArguments()[0];
-                if(handledClasses.contains(actualTypeArgument1)) {
-                    continue;
-                }
-                handledClasses.add(actualTypeArgument1);
-                schemaMap.put("$ref", resolveType(actualTypeArgument1));
+    private static void addType(final Map<String, Object> objectToAddTo, final Class type) throws CouldNotPerformException {
+        final String key = isReferencedType(type) ? "$ref" : "type";
+        objectToAddTo.put(key, resolveType(type));
+    }
+
+    private static String resolveType(final Class clazz) throws CouldNotPerformException {
+        if (isReferencedType(clazz)) {
+            String[] split = clazz.getName().split("\\$");
+            if (split.length < 1 || split.length > 3) {
+                throw new CouldNotPerformException("Unexpected split length for class[" + clazz.getName() + "]");
+            }
+
+            if (split.length == 2) {
+                return "#/components/schemas/Openbase" + split[1];
             } else {
-                if(handledClasses.contains(returnTypes.get(returnType))) {
-                    continue;
-                }
-                handledClasses.add(returnTypes.get(returnType));
-                schemaMap.put("$ref", resolveType(returnTypes.get(returnType)));
+                return "#/components/schemas/Openbase" + split[1] + "." + split[2];
             }
-            oneOf.add(schemaMap);
-            // System.out.println("Return type: " + returnTypes.get(returnType).getSimpleName() + " - " + returnType);
-        }
-
-        DumperOptions options = new DumperOptions();
-        options.setDefaultFlowStyle(DumperOptions.FlowStyle.BLOCK);
-        System.out.println(new Yaml(options).dump(pathMap));
-    }
-
-    private static String resolveType(final Class clazz) {
-        if (Message.class.isAssignableFrom(clazz)
-                || ProtocolMessageEnum.class.isAssignableFrom(clazz)) {
-            return "#/components/schemas/Openbase" + clazz.getSimpleName();
         } else if (clazz.equals(String.class)) {
             return "string";
-        } else if (clazz.equals(Boolean.class)) {
-            return "bool";
+        } else if (clazz.equals(Boolean.class) || clazz.equals(boolean.class)) {
+            return "boolean";
         } else {
             return clazz.getSimpleName();
         }
