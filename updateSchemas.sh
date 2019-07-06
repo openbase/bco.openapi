a55cc9f6eb8010bcee84fc601d24ad67adfa8ac4
Divine Threepwood <divine@openbase.org>
2019-07-06 22:11:04 +0200

    update schema path in updateSchemas.sh

--- updateSchemas.sh
+++ updateSchemas.sh
@@ -5,5 +5,5 @@ git submodule init
 git submodule update
 
 # invoke script with parameter
-./protoToOpenAPISchema/protoToOpenAPISchema.py --openAPI api/bco-openapi.yaml --overwrite --schemaPrefix Openbase -p type
+./protoToOpenAPISchema/protoToOpenAPISchema.py --openAPI src/main/resources/bco-openapi.yaml --overwrite --schemaPrefix Openbase -p type
 
